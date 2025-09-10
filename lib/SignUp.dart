c377c6fab1594afee47f3e728ecec5253cdcfca6
Keerthiraajan <keerthiraajanbtechcse@gmail.com>
2025-09-10 14:07:29 +0530

    Next Commit

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -2,7 +2,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:formula/places_services.dart';
+import 'places_services.dart';
 import 'main.dart'; // Import for ethClient, sendTransaction, etc.
 
 class SignUpPage extends StatefulWidget {
